d9b9f956a15324228fdbed13dc588c5c90e1a1b5
Archila21 <aarchila21@gmail.com>
2022-03-09 22:14:41 -0600

    New y destacada Publicaciones

--- dao/publicaciones/publicaciones.model.js
+++ dao/publicaciones/publicaciones.model.js
@@ -3,65 +3,60 @@ const getDb = require("../mongodb");
 let db = null;
 
 class Publicaciones {
-  
-  collection = null;
-  constructor() {
-    getDb()
-    .then((database) => {
-      db = database;
-        this.collection = db.collection('Publicaciones')
-        if (process.env.MIGRATE === "true") {
-          //Por si se ocupa algo
-        }
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  }
 
-  async new(nombres, apellidos, identidad, telefono, correo) {
-    const newPublicacion = {
-      nombres,
-      apellidos,
-      identidad,
-      telefono,
-      correo
-    };
-    return await this.collection.insertOne(newPublicacion);
-  }
+    collection = null;
+    constructor() {
+        getDb()
+            .then((database) => {
+                db = database;
+                this.collection = db.collection('Publicaciones')
+                if (process.env.MIGRATE === "true") {
+                    //Por si se ocupa algo
+                }
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    }
 
-  async getAll() {
-    const cursor = this.collection.find({});
-    return await cursor.toArray();
-  }
+    async new(id_usuario, contenido, fecha, destacada, likes) {
+        const newPublicacion = {
+            id_usuario,
+            contenido,
+            fecha,
+            destacada,
+            likes
+        };
+        return await this.collection.insertOne(newPublicacion);
+    }
 
-  async getById(id) {
-    const _id = new ObjectId(id);
-    const filter = {_id};
-    return await this.collection.findOne(filter);
-  }
+    async getAll() {
+        const cursor = this.collection.find({});
+        return await cursor.toArray();
+    }
 
-  async updateOne(id, nombres, apellidos, identidad, telefono, correo) {
-    const filter = { _id: new ObjectId(id)};
-    //UPDATE PACIENTES SET campo=valor, campo=valor where id = id;
-    const updateCmd = {
-      '$set':{
-        nombres,
-        apellidos,
-        identidad,
-        telefono,
-        correo
-      }
-    };
-    return await this.collection.updateOne(filter, updateCmd);
-  }
+    async getById(id) {
+        const _id = new ObjectId(id);
+        const filter = { _id };
+        return await this.collection.findOne(filter);
+    }
 
-  async deleteOne(id) {
-    const _id = new ObjectId(id);
-    const filter = {_id};
-    return await this.collection.deleteOne(filter);
-    
-  }
+    async updateOne(id, destacada) {
+        const filter = { _id: new ObjectId(id) };
+        const updateCmd = {
+            '$set': {
+                destacada,
+            }
+        };
+        return await this.collection.updateOne(filter, updateCmd);
+    }
+
+    async deleteOne(id) {
+        const _id = new ObjectId(id);
+        const filter = { _id };
+        return await this.collection.deleteOne(filter);
+
+    }
 }
 
 module.exports = Publicaciones;
--- routes/api/v1/publicaciones/publicaciones.js
+++ routes/api/v1/publicaciones/publicaciones.js
@@ -5,84 +5,79 @@ const Publicaciones = require("../../../../dao/publicaciones/publicaciones.model
 const publicacionModel = new Publicaciones();
 
 router.get("/", (req, res) => {
-  res.status(200).json({
-    endpoint: "Publicaciones",
-    updates: new Date(2022, 0, 19, 18, 41, 0),
-  });
+    res.status(200).json({
+        endpoint: "Publicaciones",
+        updates: new Date(2022, 0, 19, 18, 41, 0),
+    });
 }); //GET /
 
-router.get("/all", async (req, res) => {
-  try {
-    const rows = await publicacionModel.getAll();
-    res.status(200).json({ status: "ok", publicaciones: rows });
-  } catch (ex) {
-    console.log(ex);
-    res.status(500).json({ status: "failed" });
-  }
+router.get("/all", async(req, res) => {
+    try {
+        const rows = await publicacionModel.getAll();
+        res.status(200).json({ status: "ok", publicaciones: rows });
+    } catch (ex) {
+        console.log(ex);
+        res.status(500).json({ status: "failed" });
+    }
 });
 
-router.get("/byid/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const row = await publicacionModel.getById(id);
-    res.status(200).json({ status: "ok", pacientes: row });
-  } catch (ex) {
-    console.log(ex);
-    res.status(500).json({ status: "failed" });
-  }
-});
-
-router.post("/new", async (req, res) => {
-  const { nombres, apellidos, identidad, email, telefono } = req.body;
-  try {
-    rslt = await publicacionModel.new(
-      nombres,
-      apellidos,
-      identidad,
-      telefono,
-      email
-    );
-    res.status(200).json({
-      status: "ok",
-      result: rslt,
-    });
-  } catch (ex) {
-    console.error(ex);
-    res.status(500).json({
-      status: "failed",
-      result: {},
-    });
-  }
+router.post("/new", async(req, res) => {
+    let destacada = 0,
+        likes = 0;
+    const { contenido, fecha } = req.body;
+    try {
+        rslt = await publicacionModel.new(
+            req.user._id,
+            contenido,
+            fecha,
+            destacada,
+            likes
+        );
+        res.status(200).json({
+            status: "ok",
+            result: rslt,
+        });
+    } catch (ex) {
+        console.error(ex);
+        res.status(500).json({
+            status: "failed",
+            result: {},
+        });
+    }
 }); //POST /new
 
-router.put("/update/:id", async (req, res) => {
-  try {
-    const { nombres, apellidos, identidad, email, telefono } = req.body;
-    const { id } = req.params;
-    const result = await publicacionModel.updateOne(
-      id,
-      nombres,
-      apellidos,
-      identidad,
-      email,
-      telefono
-    );
-    res.status(200).json({ status: "ok", result });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ status: "failed" });
-  }
+
+
+router.delete("/delete/:id", async(req, res) => {
+    try {
+        const { id } = req.params;
+        const result = await publicacionModel.deleteOne(id);
+        res.status(200).json({ status: "ok", result });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ status: "failed" });
+    }
 });
 
-router.delete("/delete/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const result = await publicacionModel.deleteOne(id);
-    res.status(200).json({ status: "ok", result });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ status: "failed" });
-  }
+router.put("/actualizarDestacada/:id", async(req, res) => {
+    try {
+        let destacada;
+        const { id } = req.params;
+        const row = await publicacionModel.getById(id);
+        if (row.destacada) {
+            destacada = 0;
+        } else {
+            destacada = 1;
+        }
+        const result = await publicacionModel.updateOne(
+            id,
+            destacada
+        );
+        res.status(200).json({ status: "ok", result });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ status: "failed" });
+    }
 });
 
 module.exports = router;
