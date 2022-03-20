282967c9dbf508c867a0d16ac5ed3a946a7c0ef3
Bryanmartz972 <bryanmartz972@gmail.com>
2022-03-20 14:22:59 -0600

    CORS instalado e implementado

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
+var whiteList = (process.env.CORS_ORIGIN || 'http://localhost:3001').split(',');
+var corsOptions = {
+  origin: (origin, callback)=>{
+    if (whiteList.indexOf(origin) >= 0){
+      callback(null, true);
+    } else {
+      callback(new Error('CORS not allowed'));
+    }
+  }
+}
 
 var indexRouter = require('./routes/index');
 var apiRouter = require('./routes/api/api')
@@ -9,6 +20,7 @@ var apiRouter = require('./routes/api/api')
 var app = express();
 
 app.use(logger('dev'));
+app.use(cors(corsOptions))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
